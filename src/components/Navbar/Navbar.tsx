e1864057e0adc602f8ad7bbf96d7b88d502655c9
Weil Essaisi <weil-97@hotmail.com>
2023-03-22 11:35:16 +0100

    added locla storage

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Navbar.scss";
+
+export default function Navbar() {
+    const {currentProject} = 
+  return (
+        <nav className="navbar">Navbar<
+         <div className="navbar-logo-container">
+            <h1>Kanban</h1>
+         </div>
+     <div className="navbar-main-container">
+        <h4>Place Board</h4>
+     </div>
+     <button className="btn"> Add new </button>
+
+  )
+
+}
